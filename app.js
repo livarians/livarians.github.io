c236f13bbe4af30b3a8986a5ede97f89c9fb69fc
Mark William Firth <markwilliamfirth@gmail.com>
2015-02-03 14:54:01 +0000

    css() and hasClass() examples

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    $(".firstbutton").click(function (){
+    $("#1").click(function (){
         $("h1").addClass('stylish');
     });
 });
 
 $(document).ready(function(){
-    $(".secondbutton").click(function(){
-        $("<p>HERE!</p>").appendTo("p")
+    $("#2").click(function(){
+        $("<h1>HERE!</h1>").appendTo("p")
+    });
+});
+
+$(document).ready(function(){
+    $("#3").click(function(){
+        alert("color = " + $("span").css("color"));
+    });
+});
+
+$(document).ready(function(){
+    $("#4").click(function(){
+        alert($("h1").hasClass("stylish"));
     });
 });
