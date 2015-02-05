779bd64dc8e82b02aacc958b94a1f4d54001cd5d
anita amini <baadge29@yahoo.co.uk>
2015-02-05 17:03:56 +0000

    animations

--- app.js
+++ app.js
@@ -2,33 +2,54 @@ $(document).ready(function(){
     $("#0").click(function(){
 		document.getElementById('title').style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
     });
-});
-
-$(document).ready(function(){
     $("#1").click(function (){
         $("h1").addClass('stylish');
     });
-});
-
-$(document).ready(function(){
     $("#2").click(function(){
         $("<h1>HERE!</h1>").appendTo("p")
     });
-});
-
-$(document).ready(function(){
     $("#3").click(function(){
         alert("color = " + $("span").css("color"));
     });
-});
-
-$(document).ready(function(){
+    $("#4").click(function(){
+        alert($("h1").hasClass("stylish"));
+    });
     $("#html").click(function(){
         $("#copy").html('It copies the text!');
     });
     $("#remove").click(function(){
         $("li:last").remove();
-    })
+    });
+    
+    $('#move').click(function(){
+	$('div').animate({left:'140px'});
+    });
+    
 });
 
+
+$(function() {
+    $("#animationButton").click(function() {
+        $("#effect").animate({
+          backgroundColor: "#15e9e9",
+          color: "#fff",
+        });
+        $("#effect").animate({
+          backgroundColor: "#eed938",
+        });
+        $("#effect").animate({
+          backgroundColor: "#9b47f0",
+        });
+        $("#effect").animate({
+          backgroundColor: "#f57e0f",
+        });
+        $("#effect").animate({
+          backgroundColor: "##35f50f",
+        });
+        $("#effect").animate({
+          backgroundColor: "#2f322e",
+            color: "white"
+        });
+    });
+});
 
